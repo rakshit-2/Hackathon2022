302b4c3e6501cdd0713c3b183350eb6f7ffb9ba7
rakshit-2 <rakshit4013@gmail.com>
2022-04-17 17:47:05 +0530

    home,sidepanel,auth..

---
+++ client/src/components/info/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+import { useHistory } from 'react-router';
+import { useState } from 'react';
+const Info = (props) => 
+{
+  return (
+    <>
+    info
+    </>
+  );
+}
+
+export default Info;
---
+++ client/src/components/profile/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+import { useHistory } from 'react-router';
+import { useState } from 'react';
+const Profile = (props) => 
+{
+  return (
+    <>
+    profile
+    </>
+  );
+}
+
+export default Profile;
